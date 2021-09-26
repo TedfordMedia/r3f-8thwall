2c2850d68d5b89fa9fe83bea6331b9c7ea9e3458
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-09-26 02:47:10 +0100

    z

---
+++ build/precache-manifest.72bd17f898dcde27060a8a7614aa19cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "092f0f05a83edc439f56",
+    "url": "/static/js/main.092f0f05.chunk.js"
+  },
+  {
+    "revision": "8f271cf14d240e3f43d4",
+    "url": "/static/js/1.8f271cf1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0131ceb2c0811f49012ab8afcdd7aed7",
+    "url": "/index.html"
+  }
+];
